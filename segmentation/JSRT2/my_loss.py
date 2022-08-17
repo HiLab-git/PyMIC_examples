9804156f85113c0d8ce2e8d6913dc3d1da9a0318
taigw <wguotai@gmail.com>
2022-08-17 20:44:15 +0800

    update JSRT2
    add logging

--- segmentation/JSRT2/my_loss.py
+++ segmentation/JSRT2/my_loss.py
@@ -16,20 +16,20 @@ class MyFocalDiceLoss(nn.Module):
     def __init__(self, params):
         super(MyFocalDiceLoss, self).__init__()
         self.beta = params['MyFocalDiceLoss_beta'.lower()]
+        self.softmax = params.get('loss_softmax', True)
         assert(self.beta >= 1.0)
 
     def forward(self, loss_input_dict):
         predict = loss_input_dict['prediction']
         soft_y  = loss_input_dict['ground_truth']
-        softmax = loss_input_dict['softmax']
 
-        if(softmax):
+        if(self.softmax):
             predict = nn.Softmax(dim = 1)(predict)
         predict = reshape_tensor_to_2D(predict)
         soft_y  = reshape_tensor_to_2D(soft_y) 
 
         dice_score = get_classwise_dice(predict, soft_y, None)
-        dice_score = 0.01 + dice_score * 0.98
+        dice_score = 0.005 + dice_score * 0.99
         dice_loss  = 1.0 - torch.pow(dice_score, 1.0 / self.beta)
 
         avg_loss = torch.mean(dice_loss)   
