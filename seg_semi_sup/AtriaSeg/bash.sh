baf4abe59def7810eded363ff99434d8cd51462c
taigw <wguotai@gmail.com>
2024-01-13 20:53:09 +0800

    update config for AtriaSeg

--- seg_semi_sup/AtriaSeg/bash.sh
+++ seg_semi_sup/AtriaSeg/bash.sh
@@ -1,11 +1,11 @@
-pymic_train config/unet3d_baseline.cfg
-pymic_test  config/unet3d_baseline.cfg
+pymic_train config/unet3d_r10_baseline.cfg
+pymic_test  config/unet3d_r10_baseline.cfg
 
-pymic_train config/unet3d_em.cfg
-pymic_test  config/unet3d_em.cfg
+pymic_train config/unet3d_r10_em.cfg
+pymic_test  config/unet3d_r10_em.cfg
 
-pymic_train config/unet3d_cps.cfg
-pymic_test  config/unet3d_cps.cfg
+pymic_train config/unet3d_r10_cps.cfg
+pymic_test  config/unet3d_r10_cps.cfg
 
 pymic_eval_seg -cfg config/evaluation.cfg
 
